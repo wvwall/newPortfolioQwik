a51f12146099228222b29511fa9be640bc786add
Walter Velardo <waltervelardo@gmail.com>
2023-03-02 10:51:57 +0100

    created project

---
+++ src/routes/index.tsx
@@ -0,0 +1,17 @@
+import { component$ } from '@builder.io/qwik'
+import type { DocumentHead } from '@builder.io/qwik-city'
+import { Link } from '@builder.io/qwik-city'
+
+export default component$(() => {
+  return <div></div>
+})
+
+export const head: DocumentHead = {
+  title: 'Web Portfolio Qwik',
+  meta: [
+    {
+      name: 'description',
+      content: 'Qwik site description',
+    },
+  ],
+}
